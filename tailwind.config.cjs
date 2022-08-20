60ab11bfbc1d97b30a95fcf3e839fee8b7bdd407
Tanaca <jtanacar@gmail.com>
2022-08-20 03:24:07 -0300

    config: add colors in tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                brand: '#526eff',
+                brandHover: '#3652e3',
+                brandActive: '#233fd1',
+                errorEasy: 'rgba(242,202,202,0.72)',
+                errorJuice: '#db524e',
+                attentionEasy: 'rgba(255,215,96,0.24)',
+                attentionJuice: '#db524e',
+                successEasy: 'rgba(90, 205, 102, 0.24)',
+                successJuice: '#5acd66',
+                link: '#0086FF',
+                linkHover: '#0068C6',
+                submarineEasy: '#FAF8F3',
+                submarineJuice: '#FFCC81',
+                lightBlue: { 100: '#f5f7f9', 200: '#dfe5ec', 300: '#c8d4df' },
+            },
+        },
+    },
+    plugins: [],
 }
