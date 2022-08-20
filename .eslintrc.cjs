b2942aeca24be6bf048dcdc44fed0a868b0f8d21
Tanaca <jtanacar@gmail.com>
2022-08-20 03:06:25 -0300

    init: create project

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'standard-with-typescript',
+        'prettier',
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    },
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div>
+            <h1 className="font-bold text-2xl text-blue-900">
+                React and Tailwind with Vitejs!
+            </h1>
+        </div>
+    )
+}
+
+export default App
