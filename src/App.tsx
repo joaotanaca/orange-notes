be5e6b9a7482e4fe0c13891f4786193a7768459f
Tanaca <jtanacar@gmail.com>
2022-08-22 17:27:38 -0300

    feat: add routing and absolute paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,9 @@
-import { useState } from 'react'
-import Button from './components/atoms/Button'
-import Input from './components/atoms/Input'
+import { Suspense } from 'react'
+import { useRoutes } from 'react-router'
+import routes from '~react-pages'
 
 function App() {
-    const [email, setEmail] = useState('')
-    const [type, setType] = useState<keyof typeof types>('email')
-    const types = {
-        email: 'translate-x-0',
-        password: '-translate-x-[67%]',
-    }
-    return (
-        <div className="relative h-screen flex justify-center p-10">
-            {type === 'password' && (
-                <a
-                    onClick={() => {
-                        setType('email')
-                    }}
-                    className="absolute top-10 left-10 text-[14px] text-link cursor-pointer"
-                >
-                    Voltar
-                </a>
-            )}
-
-            <a
-                className="absolute top-10 right-10 underline text-[14px] text-link"
-                href="/"
-            >
-                Create a Account
-            </a>
-            <form
-                onSubmit={event => {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }}
-                className="w-full overflow-hidden flex flex-col justify-center gap-10"
-            >
-                <div className="flex flex-col gap-3">
-                    <h1 className="text-4xl font-semibold tracking-[0.05rem]">
-                        Log In
-                    </h1>
-                    <h2 className="text-sm text-black text-opacity-70 tracking-wide">
-                        {type === 'password' && email ? (
-                            <>
-                                Useing <b>{email}</b> to log in
-                            </>
-                        ) : (
-                            "What's your email?"
-                        )}
-                    </h2>
-                </div>
-                <div
-                    className={`flex justify-between w-[300%] mt-3 transition-all duration-500 ${types[type]}`}
-                >
-                    <Input
-                        className="w-[33%]"
-                        type="email"
-                        placeholder="YOUR EMAIL"
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                    <Input
-                        className="w-[33%]"
-                        type="password"
-                        placeholder="YOUR PASSWORD"
-                    />
-                </div>
-
-                <Button
-                    type="submit"
-                    onClick={() => {
-                        if (type === 'email') setType('password')
-                    }}
-                    className="w-full"
-                >
-                    Continue
-                </Button>
-            </form>
-        </div>
-    )
+    return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react(), tsconfigPaths(), Pages({ dirs: './src/pages' })],
 })
