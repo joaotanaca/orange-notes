8f1f37ba51517db5b9394e7075201d73c24725f9
Tanaca <jtanacar@gmail.com>
2022-08-22 17:26:24 -0300

    refactor: input component with forwardRef

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -1,36 +1,42 @@
-import { createRef, useState } from 'react'
+import { forwardRef, useState } from 'react'
 
 type Props = React.DetailedHTMLProps<
     React.InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
 >
 
-const Input = ({ className, onChange, ...props }: Props) => {
-    const [text, setText] = useState('')
-    return (
-        <div className={`relative ${className}`}>
-            <label
-                className={`-z-[1] absolute transition-all text-black left-4 outline-0 ${
-                    text
-                        ? 'bottom-14 text-opacity-60 text-xs'
-                        : 'bottom-4 text-opacity-30 text-sm'
-                }`}
-                htmlFor={props.name}
-            >
-                {props.placeholder}
-            </label>
-            <input
-                {...props}
-                className="p-4 z-10 bg-transparent border-b-[1px] border-b-[rgba(17, 17, 17, 0.32)] focus:outline-none placeholder:text-transparent w-full"
-                onChange={event => {
-                    event.preventDefault()
-                    event.stopPropagation()
-                    setText(event.currentTarget.value)
-                    onChange?.(event)
-                }}
-            />
-        </div>
-    )
-}
+const Input = forwardRef<HTMLInputElement, Props>(
+    ({ className, onChange, ...props }, ref) => {
+        const [text, setText] = useState('')
+        const handleChangeText = (
+            event: React.ChangeEvent<HTMLInputElement>
+        ) => {
+            event.preventDefault()
+            event.stopPropagation()
+            setText(event.target.value)
+            onChange?.(event)
+        }
+        return (
+            <div className={`relative ${className}`}>
+                <label
+                    className={`-z-[1] absolute transition-all text-black left-4 outline-0 ${
+                        text
+                            ? 'bottom-14 text-opacity-60 text-xs'
+                            : 'bottom-4 text-opacity-30 text-sm'
+                    }`}
+                    htmlFor={props.name}
+                >
+                    {props.placeholder}
+                </label>
+                <input
+                    ref={ref}
+                    className="p-4 z-10 bg-transparent border-b-[1px] border-b-[rgba(17, 17, 17, 0.32)] focus:outline-none placeholder:text-transparent w-full"
+                    onChange={handleChangeText}
+                    {...props}
+                />
+            </div>
+        )
+    }
+)
 
 export default Input
