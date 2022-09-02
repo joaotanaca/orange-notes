faa494421e42bfc07501a8926d03c47019b4e4b6
Tanaca <jtanacar@gmail.com>
2022-09-02 01:20:23 -0300

    feat: add toastfy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import { useRoutes } from 'react-router'
 import routes from '~react-pages'
+import 'react-toastify/dist/ReactToastify.min.css'
 
 function App() {
     return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>
